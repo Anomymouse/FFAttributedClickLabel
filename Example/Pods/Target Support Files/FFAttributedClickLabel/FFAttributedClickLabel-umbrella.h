79a1a6aca1ffaf2430423ca5d5d28961cffdfe9a
Mr.Yao <348892914@qq.com>
2016-05-16 19:55:58 +0800

    xxx

--- Example/Pods/Target Support Files/FFAttributedClickLabel/FFAttributedClickLabel-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double FFAttributedClickLabelVersionNumber;
-FOUNDATION_EXPORT const unsigned char FFAttributedClickLabelVersionString[];
-
--- Example/Pods/Target Support Files/Pods-FFAttributedClickLabel_Tests/Pods-FFAttributedClickLabel_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FFAttributedClickLabel_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_FFAttributedClickLabel_Tests
-@end
